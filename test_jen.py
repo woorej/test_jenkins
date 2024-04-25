1a22b2f12ea9915db61374cef984c085c3da6c14
woorej <wjk5848@gmail.com>
2024-04-25 09:58:20 +0900

    added moltople test code

--- test_jen.py
+++ test_jen.py
@@ -1,4 +1,4 @@
-from jen import add_numbers, subtrack_numbers
+from jen import add_numbers, subtrack_numbers, multiple_numbers
 import pytest
 
 @pytest.mark.parametrize("n1, n2, expected_result",[
@@ -18,3 +18,12 @@ def test_add_numbers(n1, n2, expected_result):
 ])
 def test_subtrack_numbers(n1, n2, expected_result):
     assert subtrack_numbers(n1, n2) == expected_result
+
+@pytest.mark.parametrize("n1, n2, expected_result",[
+    (2, 3, 6),
+    (4, 5, 20),
+    (-1, 1, -1),
+    (0, 0, 0)
+])
+def test_multiple_numbers(n1, n2, expected_result):
+    assert multiple_numbers(n1, n2) == expected_result
